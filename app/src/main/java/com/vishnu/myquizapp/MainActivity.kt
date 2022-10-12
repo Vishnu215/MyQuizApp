7168ba3fc7fd626ecacc24adb7000b0d6fef50aa
vishnu G <vishnug2903@gmail.com>
2022-10-12 19:16:57 +0530

    Updated project

--- app/src/main/java/com/vishnu/myquizapp/MainActivity.kt
+++ app/src/main/java/com/vishnu/myquizapp/MainActivity.kt
@@ -3,30 +3,24 @@ package com.vishnu.myquizapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnStart: Button = findViewById(R.id.btn_start)
-        val etName: EditText = findViewById(R.id.et_name)
-        btnStart.setOnClickListener {
-
-            if (etName.text.isEmpty()) {
-                Toast.makeText(
-                    this,
-                    "Please enter your name", Toast.LENGTH_LONG
-                ).show()
+        val buttonStart: Button = findViewById(R.id.btn_start)
+        val etName: AppCompatEditText = findViewById(R.id.et_name)
+        buttonStart.setOnClickListener {
+            if (etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show()
             } else {
-                // Intent(my_Current_Activity, to_other_activity_which_you_want_to_move)
-                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
                 intent.putExtra(Constants.USER_NAME, etName.text.toString())
                 startActivity(intent)
-                // if you want to end the current activity then finish()
                 finish()
             }
         }
--- app/src/main/java/com/vishnu/myquizapp/ResultActivity.kt
+++ app/src/main/java/com/vishnu/myquizapp/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.vishnu.myquizapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,15 +13,21 @@ class ResultActivity : AppCompatActivity() {
 
         val tvName: TextView = findViewById(R.id.tv_name)
         val tvScore: TextView = findViewById(R.id.tv_score)
-        val btnFinish: TextView = findViewById(R.id.btn_finish)
+        val btnFinish: Button = findViewById(R.id.btn_finish)
 
-        tvName.text = intent.getStringExtra(Constants.USER_NAME)
+        val userName = intent.getStringExtra(Constants.USER_NAME)
+        tvName.text = userName
+
         val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
-        val correctAnswers = intent.getIntExtra(Constants.CORRECT_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
 
-        tvScore.text = "Your score is $correctAnswers out of $totalQuestions"
+        tvScore.text = "Your Score is $correctAnswers out of $totalQuestions."
+
         btnFinish.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            //
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
         }
     }
+
+
 }
